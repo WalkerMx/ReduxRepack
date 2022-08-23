cec4bcae1141af781413294b66fc974377f1c803
Walker <33885782+WalkerMx@users.noreply.github.com>
2022-08-23 15:32:42 -0400

    Add files via upload

---
+++ DOA_REDUX_REPACK/FilePatcher.vb
@@ -0,0 +1,60 @@
+﻿Imports System.IO
+Imports System.IO.Compression
+
+Public Class FilePatcher
+
+    Dim ArmorPatchList As New List(Of Patch)
+
+    Public Structure Patch
+        Public Property PatchesList As String()
+        Public Property SourceFile As String
+        Public Sub New(Patches As String(), Source As String)
+            PatchesList = Patches
+            SourceFile = Source
+        End Sub
+    End Structure
+
+    Public Sub New()
+
+        ArmorPatchList.AddRange({New Patch({"1950scasualvariantfemale.xd3", "1950scasualvariantfemaleold.xd3", "1950scasualoldfemale.xd3"}, "\meshes\doa\armor\1950stylecasual01\f\outfitf.nif"),
+                            New Patch({"1950scasual02oldf.xd3", "1950scasual2variantfold.xd3"}, "\meshes\doa\armor\1950stylecasual02\outfitf.nif"),
+                            New Patch({"1950ssuitdarkf.xd3"}, "\meshes\doa\armor\1950stylesuit\f\outfitf.nif"),
+                            New Patch({"rivetcitysecurityuniformf.xd3", "taloncompanyf.xd3", "tenpennysecurityuniformf.xd3", "rileysrangersuniformf.xd3"}, "\meshes\doa\armor\combatarmor\f\outfitf.nif"),
+                            New Patch({"rivetcityscientistf.xd3"}, "\meshes\doa\armor\doctorli\f\outfitf.nif"),
+                            New Patch({"leopardprintlingerie.xd3"}, "\meshes\doa\armor\lingerie\outfitf.nif"),
+                            New Patch({"redjumpsuitf.xd3", "redracerjumpsuitf.xd3"}, "\meshes\doa\armor\robcojumpsuit\outfitf.nif"),
+                            New Patch({"vaultsuit108utilitydadf.xd3"}, "\meshes\doa\armor\vaultsuitutility\f\vaultsuitutilitydadf.nif"),
+                            New Patch({"wastelanddoctor02f.xd3"}, "\meshes\doa\armor\wastelanddoctor01\outfitf.nif")})
+
+    End Sub
+
+    Public Sub PatchAllFiles(SourceFolder As String, DestFolder As String)
+        Dim TempFolder As String = Path.GetTempPath & Path.GetRandomFileName
+        Directory.CreateDirectory(TempFolder)
+        File.WriteAllBytes(TempFolder & "\Delta.zip", My.Resources.Delta)
+        ZipFile.ExtractToDirectory(TempFolder & "\Delta.zip", TempFolder)
+        For Each PatchSet As Patch In ArmorPatchList
+            For Each XDeltaPatch As String In PatchSet.PatchesList
+                Dim Filename As String = Path.GetFileName(PatchSet.SourceFile)
+                Dim NewFolderPath As String = Directory.GetParent(DestFolder & PatchSet.SourceFile).FullName & "\" & XDeltaPatch.Replace(".xd3", "")
+                Directory.CreateDirectory(NewFolderPath)
+                Form1.RunXDelta(Form1.Mode.Unpack, DestFolder & PatchSet.SourceFile, NewFolderPath & "\" & Filename, TempFolder & "\" & XDeltaPatch)
+            Next
+        Next
+
+        Form1.RunXDelta(Form1.Mode.Unpack, SourceFolder & "\meshes\characters\_male\daughtersofares\eye\eyelefthumanfemale.nif", DestFolder & "\meshes\characters\_male\daughtersofares\eye\eyelefthumanfemale.nif", TempFolder & "\eyelefthumanfemale.xd3")
+        Form1.RunXDelta(Form1.Mode.Unpack, SourceFolder & "\meshes\characters\_male\daughtersofares\eye\eyerighthumanfemale.nif", DestFolder & "\meshes\characters\_male\daughtersofares\eye\eyerighthumanfemale.nif", TempFolder & "\eyerighthumanfemale.xd3")
+        Form1.RunXDelta(Form1.Mode.Unpack, SourceFolder & "\textures\characters\daughtersofares\eye\green.dds", DestFolder & "\textures\characters\daughtersofares\eye\green.dds", TempFolder & "\greenold-green.xd3")
+        Form1.RunXDelta(Form1.Mode.Unpack, DestFolder & "\textures\characters\daughtersofares\eye\green.dds", DestFolder & "\textures\characters\daughtersofares\eye\blue.dds", TempFolder & "\green-blue.xd3")
+        Form1.RunXDelta(Form1.Mode.Unpack, DestFolder & "\textures\characters\daughtersofares\eye\green.dds", DestFolder & "\textures\characters\daughtersofares\eye\indigo.dds", TempFolder & "\green-indigo.xd3")
+        Form1.RunXDelta(Form1.Mode.Unpack, DestFolder & "\textures\characters\daughtersofares\eye\green.dds", DestFolder & "\textures\characters\daughtersofares\eye\orange.dds", TempFolder & "\green-orange.xd3")
+        Form1.RunXDelta(Form1.Mode.Unpack, DestFolder & "\textures\characters\daughtersofares\eye\green.dds", DestFolder & "\textures\characters\daughtersofares\eye\pink.dds", TempFolder & "\green-pink.xd3")
+        Form1.RunXDelta(Form1.Mode.Unpack, DestFolder & "\textures\characters\daughtersofares\eye\green.dds", DestFolder & "\textures\characters\daughtersofares\eye\red.dds", TempFolder & "\green-red.xd3")
+        Form1.RunXDelta(Form1.Mode.Unpack, DestFolder & "\textures\characters\daughtersofares\eye\green.dds", DestFolder & "\textures\characters\daughtersofares\eye\violet.dds", TempFolder & "\green-violet.xd3")
+        Form1.RunXDelta(Form1.Mode.Unpack, DestFolder & "\textures\characters\daughtersofares\eye\green.dds", DestFolder & "\textures\characters\daughtersofares\eye\white.dds", TempFolder & "\green-white.xd3")
+        Form1.RunXDelta(Form1.Mode.Unpack, DestFolder & "\textures\characters\daughtersofares\eye\green.dds", DestFolder & "\textures\characters\daughtersofares\eye\yellow.dds", TempFolder & "\green-yellow.xd3")
+
+        Directory.Delete(TempFolder, True)
+    End Sub
+
+End Class
---
+++ DOA_REDUX_REPACK/Form1.vb
@@ -0,0 +1,143 @@
+﻿Imports System.IO
+Imports DOA_REDUX_REPACK.Form1.Mode
+
+Public Class Form1
+
+    Public Enum Mode
+        Pack
+        Unpack
+    End Enum
+
+    Dim Mapper As New FileMapper
+    Dim Patcher As New FilePatcher
+    Dim RootPath As String = Environment.CurrentDirectory
+
+    Private Sub CompressCheckbox_CheckedChanged(sender As Object, e As EventArgs) Handles CompressCheckbox.CheckedChanged
+        If CompressCheckbox.Checked = True Then
+            If MessageBox.Show("Compression can cause stutter! Are you sure?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) = MsgBoxResult.No Then
+                CompressCheckbox.Checked = False
+            End If
+        End If
+    End Sub
+
+    Private Sub StartButton_Click(sender As Object, e As EventArgs) Handles StartButton.Click
+
+        Dim RequiredFiles As String() = {"BSArch.exe", "Daughters of Ares.bsa", "xdelta3.exe"}
+
+        For Each FileName As String In RequiredFiles
+            If Not File.Exists(RootPath & "\" & FileName) Then
+                MsgBox(FileName & " is missing!", MsgBoxStyle.Exclamation, "Error")
+                Exit Sub
+            End If
+        Next
+
+        RepackFiles()
+
+    End Sub
+
+    Private Sub RepackFiles()
+        StartButton.Enabled = False
+        Dim SourceFolder As String = RootPath & "\TempSource"
+        Dim DestFolder As String = RootPath & "\TempREDUX"
+        Directory.CreateDirectory(SourceFolder)
+        Directory.CreateDirectory(DestFolder)
+
+        RunBSArch(Unpack, SourceFolder)
+
+        ProgressBar1.Value = 20
+
+        My.Computer.FileSystem.CopyDirectory(SourceFolder & "\meshes\characters", DestFolder & "\meshes\characters")
+        My.Computer.FileSystem.CopyDirectory(SourceFolder & "\textures", DestFolder & "\textures")
+        Dim SourceFiles = Directory.GetFiles(SourceFolder & "\meshes", "*.nif", SearchOption.AllDirectories)
+
+        For Each MeshPath As String In SourceFiles
+            If Mapper.GetFileMatch(MeshPath, DestFolder) IsNot Nothing Then
+                My.Computer.FileSystem.CopyFile(MeshPath, Mapper.GetFileMatch(MeshPath, DestFolder))
+            End If
+        Next
+
+        ProgressBar1.Value = 30
+
+        File.Delete(DestFolder & "\meshes\characters\_male\daughtersofares\eye\eyelefthumanfemale.nif")
+        File.Delete(DestFolder & "\meshes\characters\_male\daughtersofares\eye\eyerighthumanfemale.nif")
+        Directory.Delete(DestFolder & "\textures\characters\daughtersofares\eye", True)
+        Directory.CreateDirectory(DestFolder & "\textures\characters\daughtersofares\eye")
+        File.Copy(SourceFolder & "\textures\characters\daughtersofares\eye\green_g.dds", DestFolder & "\textures\characters\daughtersofares\eye\eye_g.dds")
+        File.Copy(SourceFolder & "\textures\characters\daughtersofares\eye\eye_n.dds", DestFolder & "\textures\characters\daughtersofares\eye\eye_n.dds")
+
+        ProgressBar1.Value = 40
+
+        For Each HairTex As String In {"01hair", "c13", "c24", "rd01", "rd11"}
+            If File.Exists(DestFolder & "\textures\characters\daughtersofares\hair\" & HairTex & ".dds") Then
+                File.Delete(DestFolder & "\textures\characters\daughtersofares\hair\" & HairTex & ".dds")
+            End If
+            If File.Exists(DestFolder & "\textures\characters\daughtersofares\hair\" & HairTex & "_hl.dds") Then
+                File.Delete(DestFolder & "\textures\characters\daughtersofares\hair\" & HairTex & "_hl.dds")
+            End If
+            If File.Exists(DestFolder & "\textures\characters\daughtersofares\hair\" & HairTex & "_n.dds") Then
+                File.Delete(DestFolder & "\textures\characters\daughtersofares\hair\" & HairTex & "_n.dds")
+            End If
+        Next
+
+        ProgressBar1.Value = 50
+
+        For Each HairMesh As String In {"c13", "c24", "hair3a", "hair3ahat", "hair3anohat", "rd01", "rd11"}
+            If File.Exists(DestFolder & "\meshes\characters\_male\daughtersofares\hair\" & HairMesh & ".nif") Then
+                File.Delete(DestFolder & "\meshes\characters\_male\daughtersofares\hair\" & HairMesh & ".nif")
+            End If
+            If File.Exists(DestFolder & "\meshes\characters\_male\daughtersofares\hair\" & HairMesh & ".egm") Then
+                File.Delete(DestFolder & "\meshes\characters\_male\daughtersofares\hair\" & HairMesh & ".egm")
+            End If
+        Next
+
+        ProgressBar1.Value = 60
+
+        Patcher.PatchAllFiles(SourceFolder, DestFolder)
+
+        For Each ThumbDb As String In Directory.GetFiles(DestFolder, "*thumbs.db", SearchOption.AllDirectories)
+            File.Delete(ThumbDb)
+        Next
+
+        ProgressBar1.Value = 80
+
+        RunBSArch(Pack, DestFolder)
+
+        ProgressBar1.Value = 100
+
+        Directory.Delete(SourceFolder, True)
+        Directory.Delete(DestFolder, True)
+        MsgBox("The operation completed successfully.", MsgBoxStyle.OkOnly, "Information") : ProgressBar1.Value = 0
+        StartButton.Enabled = True
+    End Sub
+
+    Private Sub RunBSArch(PackMode As Mode, FolderPath As String)
+        Dim Args As String = ""
+        Select Case PackMode
+            Case Pack
+                Args = "pack " & Quote(FolderPath) & " " & Quote(RootPath & "\DoA-Redux.bsa") & " -fnv -mt" & IIf(CompressCheckbox.Checked = True, " -z", "")
+            Case Unpack
+                Args = "unpack " & Quote(RootPath & "\Daughters of Ares.bsa") & " " & Quote(FolderPath) & " -mt -q"
+        End Select
+        Dim BuildProcess As ProcessStartInfo = New ProcessStartInfo(RootPath & "\BSArch.exe", Args) With {.WorkingDirectory = RootPath, .RedirectStandardOutput = True, .RedirectStandardError = True, .UseShellExecute = False, .CreateNoWindow = True}
+        Dim BatchProcess As Process = Process.Start(BuildProcess)
+        BatchProcess.WaitForExit()
+    End Sub
+
+    Public Sub RunXDelta(PackMode As Mode, SourcePath As String, DestPath As String, XD3Path As String)
+        Dim Args As String = ""
+        Select Case PackMode
+            Case Pack
+                Args = "-A= -s " & Quote(SourcePath) & " " & Quote(DestPath) & " " & Quote(XD3Path)
+            Case Unpack
+                Args = "-d -s " & Quote(SourcePath) & " " & Quote(XD3Path) & " " & Quote(DestPath)
+        End Select
+        Dim BuildProcess As ProcessStartInfo = New ProcessStartInfo(RootPath & "\xdelta3.exe", Args) With {.WorkingDirectory = RootPath, .RedirectStandardOutput = True, .RedirectStandardError = True, .UseShellExecute = False, .CreateNoWindow = True}
+        Dim BatchProcess As Process = Process.Start(BuildProcess)
+        BatchProcess.WaitForExit()
+    End Sub
+
+    Public Function Quote(Source As String)
+        Return """"c & Source & """"c
+    End Function
+
+End Class
